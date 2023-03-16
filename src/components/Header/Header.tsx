df4135a90270c0527a3e82d51bec9e9a73f231fd
GuilhermeDevv <ddc3.0cf1@gmail.com>
2023-03-15 22:03:20 -0300

    criacao da pagina de components

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Container, Content } from "./styles";
+export function Header() {
+    return (
+        <Container>
+            <Content>teste</Content>
+        </Container>
+    );
+}
