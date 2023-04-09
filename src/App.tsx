23b70acba925d725b138e84f533cc33c920519a3
GuilhermeDevv <ddc3.0cf1@gmail.com>
2023-04-09 14:59:30 -0300

    inset components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { Header } from "./components/Header/Header";
+import { AboutMe } from "./components/AboutMe/AboutMe";
+import { MyWorks } from "./components/MyWorks/MyWorks";
+import { MoreAbout } from "./components/MoreAbout/MoreAbout";
+import { Footer } from "./components/Footer/Footer";
+import { ButtonWpp } from "./components/ButtonWpp/ButtonWpp";
 
 function App() {
-	return <></>;
+    return (
+        <>
+            <ButtonWpp />
+            <Header />
+            <AboutMe />
+            <MyWorks />
+            <MoreAbout />
+            <Footer />
+        </>
+    );
 }
 
 export default App;
